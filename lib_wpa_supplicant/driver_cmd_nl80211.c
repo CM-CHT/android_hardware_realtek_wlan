6d00150fc29a0bcbb8909ab27bea24a62dcadc6d
Konsta <konsta09@gmail.com>
2016-10-30 17:34:05 +0200

    include driver_nl80211.h from wpa_supplicant
    Change-Id: I441ead36bb6e8a43f8ec46ab1ddc548b1745e809

--- lib_wpa_supplicant/driver_cmd_nl80211.c
+++ lib_wpa_supplicant/driver_cmd_nl80211.c
@@ -10,6 +10,13 @@
  *
  */
 
+#include "includes.h"
+#include <sys/types.h>
+#include <fcntl.h>
+#include <net/if.h>
+
+#include "common.h"
+#include "linux_ioctl.h"
 #include "driver_nl80211.h"
 #include "wpa_supplicant_i.h"
 #include "config.h"
